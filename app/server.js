9dd4569c0b421e2172a54bb6a3d649b694dab01a
Ramona Meunier <gramyy7@gmail.com>
2021-04-12 13:37:19 +0200

    starting form, fixing up server with serving static files

--- app/server.js
+++ app/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const serveStatic = require('serve-static');
+
 const PORT = 8080;
 
-app.use(express.static('static'));
+app.use('/static', serveStatic(path.join(__dirname, 'static')));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname+ '/index.html');
